d1dd52b2f5e9be5f9eb30c26f105280348994e68
Kazeera <kazeeraaliar@gmail.com>
2021-04-23 14:23:23 -0400

    Updated Comparisons tab
    - Made "Make Comparisons" table with ComparisonTable and ComparisonTableRow class definitions
    - Moved kv and py files to App folder

--- App/main.py
+++ App/main.py
@@ -5,45 +5,35 @@ from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem, TabbedPanelHeader
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
-from kivy.uix.dropdown import DropDown
 from kivy.base import runTouchApp
 from kivy.uix.widget import Widget
-
-
-
-class CustomDropDown(DropDown):
-    pass
+from kivy.uix.boxlayout import BoxLayout
 
 class UploadTable(Widget):
     pass
 
-class ComparisonTable(Widget):
+class ComparisonTableRow(BoxLayout):
     pass
 
-class CustomizeStats(Widget):
+class ComparisonTable(BoxLayout):
+    def add_a_row(self):
+        self.ids.container.add_widget(ComparisonTableRow())
+
+class AdvancedOptions(Widget):
     def correlation_clicked(self, value):
         self.ids.correlation_label.text = f'{value}'
 
     def test_clicked(self, value):
         self.ids.test_label.text = f'{value}'
 
+
 class KVTabLay(Widget):
     pass
 
 
-
 class HourglassApp(App):
     def build(self):
-
-        # dropdown = CustomDropDown()
-        # mainbutton = Button(text='Select',size_hint = (0.5,0.1))
-        #                     #pos_hint = (0.5, 0.5)?
-        # mainbutton.bind(on_release=dropdown.open)
-        # dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-
         return KVTabLay()
 
 if __name__ == '__main__':
     HourglassApp().run()
-
-#Test Change - Bowen
