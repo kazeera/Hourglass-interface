7433e500400872bd029fc5c3c60dc78ff50a0870
raj-w <henry.r.waterhouse@gmail.com>
2021-04-30 14:22:32 -0400

    Added FileChooser module

--- App/main.py
+++ App/main.py
@@ -1,17 +1,29 @@
+import os
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem, TabbedPanelHeader
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivy.base import runTouchApp
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.popup import Popup
 
 class UploadTable(Widget):
-    pass
+    def popup_opener(self):
+        open_popup()
+
+    def button_changer(self, key):
+        if key == 'NumMatButton':
+            self.ids.NumMatButton.text = f'{NumericMatrix}'
+        if key == 'RowAnnButton':
+            self.ids.RowAnnButton.text = f'{RowAnn}'
+        if key == 'ColAnnButton':
+            self.ids.ColAnnButton.text = f'{ColAnn}'
 
 class ComparisonTableRow(BoxLayout):
     pass
@@ -44,6 +56,29 @@ class AdvancedOptions(Widget):
 class KVTabLay(Widget):
     pass
 
+class FileChooserPopup(FloatLayout):
+    global NumericMatrix
+    global RowAnn
+    global ColAnn
+
+    NumericMatrix = StringProperty()
+    RowAnn = StringProperty()
+    ColAnn = StringProperty()
+
+    def selected(self, filepath): #pass button id?
+        NumericMatrix = filepath[0]
+        print(NumericMatrix)
+        close_popup()
+
+def open_popup():
+    file_chooser = FileChooserPopup()
+    global popup_window
+    popup_window = Popup(title='Upload a File', content=file_chooser, size_hint=(0.75, 0.75), auto_dismiss=False)
+    popup_window.open()
+
+
+def close_popup():
+    popup_window.dismiss()
 
 class HourglassApp(App):
     def build(self):
